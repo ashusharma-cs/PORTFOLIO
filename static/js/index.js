ae5a4666593fb01290d932ed226453fb07945df5
Ashu <ashu.lucifer@gmail.com>
2021-01-13 16:13:59 +0530

    INITIAL COMMIT

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+var header = document.querySelector('#navbar');
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+}); 
